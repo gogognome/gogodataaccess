75e5890aa26f104a8bb8f8f35f7305cb4c4111a8
Sander Kooijmans <sander.kooijmans@philips.com>
2015-08-30 08:08:38 +0200

    Ensure that in-memory database exists throughout the complete test. As long as at least one connection to the in-memory database exists, the database will not get deleted.

--- src/test/java/nl/gogognome/dataaccess/dao/BaseInMemTransactionTest.java
+++ src/test/java/nl/gogognome/dataaccess/dao/BaseInMemTransactionTest.java
@@ -7,11 +7,13 @@ import org.h2.jdbcx.JdbcDataSource;
 import org.junit.After;
 import org.junit.Before;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 
 public class BaseInMemTransactionTest {
 
     private static int uniqueId = 0;
+    private Connection ensureInMemDatabaseExistsConnection;
 
     @Before
     public void createInMemoryDatabase() throws DataAccessException, SQLException {
@@ -20,13 +22,20 @@ public class BaseInMemTransactionTest {
         uniqueId++;
 
         CompositeDatasourceTransaction.registerDataSource("test", dataSource);
+        ensureInMemDatabaseExistsConnection = dataSource.getConnection();
 
         CurrentTransaction.create();
     }
 
     @After
-    public void closeTransaction() throws DataAccessException {
-        CurrentTransaction.close(false);
+    public void closeTransaction() throws DataAccessException, SQLException {
+        try {
+            if (ensureInMemDatabaseExistsConnection != null) {
+                ensureInMemDatabaseExistsConnection.close();
+            }
+        } finally {
+            CurrentTransaction.close(false);
+        }
     }
 
 }
