d3ca7e0f77b3aecdf79b30747dd8c274d0ef54b2
Sander Kooijmans <sander@gogognome.nl>
2016-10-24 19:34:34 +0200

    Added Javadoc.

--- src/main/java/nl/gogognome/dataaccess/transaction/Transaction.java
+++ src/main/java/nl/gogognome/dataaccess/transaction/Transaction.java
@@ -2,6 +2,11 @@ package nl.gogognome.dataaccess.transaction;
 
 import nl.gogognome.dataaccess.DataAccessException;
 
+/**
+ * This interface specifies a transaction. A transaction is started by creating an instance of Transaction.
+ * To finish the transaction either call commit() or rollback().
+ * Finally, close() must be called on the transaction. A Transaction instance can be used for one transaction.
+ */
 public interface Transaction {
 
     void commit() throws DataAccessException;
